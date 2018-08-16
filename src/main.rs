bdf3386a4d3112c2ef87e79566067b5c140af20f
Callum Dunster <calonthexbox@hotmail.com>
2018-08-16 23:02:12 +0200

    Generate random answer.
    Use rand crate to generate random number between 1 and 100 and print it.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+extern crate rand;
+
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
+
+    let answer = rand::thread_rng().gen_range(1, 101);
+
+    println!("answer is: {}", answer);
+
     println!("Please enter your guess.");
 
     let mut guess = String::new();
